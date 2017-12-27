cd80f9bdd16d80c66f4612b35b85980eb35519a4
Guilherme Labrego <glabrego@gmail.com>
2017-12-27 16:24:59 -0200

    Fix constant error, change FactoryGirl to FactoryBot

--- spec/factories/evaluations.rb
+++ spec/factories/evaluations.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-FactoryGirl.define do
+FactoryBot.define do
   factory :evaluation do
     r1 1
     r2 1
--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-FactoryGirl.define do
+FactoryBot.define do
   factory :game do
     name 'MyString'
     description 'MyString'
